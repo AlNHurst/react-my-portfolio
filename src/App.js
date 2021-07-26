fd9becf9bc39688a12b1236796168eda39125167
Alexandra Hurst <82131699+AlNHurst@users.noreply.github.com>
2021-07-26 15:06:51 -0400

    deploying to Heroku

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import './App.css';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Main from './components/Main';
-import { Link } from 'react-router-dom';
-import { Container, Nav } from 'react-bootstrap';
+import { BrowserRouter } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 
@@ -12,9 +11,11 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
-      <NavBar/>
-        <Main/>
-      <Footer/>
+      <BrowserRouter>
+        <NavBar />
+        <Main />
+        <Footer />
+      </BrowserRouter>
     </>
   );
 }
